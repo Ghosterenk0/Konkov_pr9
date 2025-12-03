a385ded0e5a4c44ebce7a9f90af697ee5e966ffe
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-12-03 11:43:05 +0500

    Update Konkov_pr9.cpp

--- Konkov_pr9/Konkov_pr9.cpp
+++ Konkov_pr9/Konkov_pr9.cpp
@@ -2,18 +2,27 @@
 #include <Windows.h>
 #include <conio.h>
 
+
+STARTUPINFO si;
+PROCESS_INFORMATION piApp;
+
+DWORD IDt[2] = {1, 2};
+HANDLE threads[2];
+
+int menu();
+
 void incCount() {
 	int count = 0;
 	while (true) {
 		std::cout << count << std::endl;
-		Sleep(3000);
 		count++;
+		Sleep(3000);
+		if (menu() == 0)
+			break;
 	}
 }
 
 void StartAndFinishProcess(const wchar_t* nameProcess) {
-	STARTUPINFO si;
-	PROCESS_INFORMATION piApp;
 
 	ZeroMemory(&si, sizeof(STARTUPINFO));
 	si.cb = sizeof(STARTUPINFO);
@@ -25,67 +34,70 @@ void StartAndFinishProcess(const wchar_t* nameProcess) {
 		std::cout << "Дочерний процесс не создан" << std::endl;
 		_getch();
 	}
-	std::cout << "Дочерний процесс создан" << std::endl;
-	while (true) {
-		char c;
-		std::cout << "Введите d чтобы завершить процесс: ";
-		std::cin >> c;
-		if (c == 'd') {
-			TerminateProcess(piApp.hProcess, 1);
-			std::cout << "Процесс завершен" << std::endl;
-			break;
-		}
+	else {
+		std::cout << "Дочерний процесс создан" << std::endl;
 	}
 
-	CloseHandle(piApp.hThread);
-	CloseHandle(piApp.hProcess);
 }
 
 void StartThread(const wchar_t* nameProcess) {
 
 	wchar_t process[256];
 	wcscpy_s(process, nameProcess);
+	threads[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartAndFinishProcess, process, 0, &IDt[0]);
+	Sleep(1000);
+	threads[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)incCount, NULL, 0, &IDt[1]);
+	
+	WaitForSingleObject(threads[0], INFINITE);
+	WaitForSingleObject(threads[1], INFINITE);
 
-	DWORD IDPotokSp;
-	HANDLE potocSp = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartAndFinishProcess, process, 0, &IDPotokSp);
-
-	DWORD IDPotoc;
-	HANDLE potokinc = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)incCount, NULL, 0, &IDPotoc);
-
-	WaitForSingleObject(potocSp, INFINITE);
-	WaitForSingleObject(potokinc, INFINITE);
 }
 
+int menu() {
+	while (true) {
+		char c;
+		std::cin >> c;
+		switch (c)
+		{
+		case '1':
+			StartThread(L"winword.exe ");
+			break;
+		case '2':
+			StartThread(L"excel.exe ");
+			break;
+		case '3':
+			StartThread(L"mspaint.exe ");
+			break;
+		case '4':
+			StartThread(L"notepad.exe ");
+			break;
+		case 'd':
+			TerminateThread(threads[0], 2);
+			TerminateThread(threads[1], 3);
+			TerminateProcess(piApp.hProcess, 1);
+			CloseHandle(piApp.hThread);
+			CloseHandle(piApp.hProcess);
+			std::cout << "Потоки завершины " << std::endl;
+			std::cout << "Программа завершена" << std::endl;
+			return 0;
+		default:
+			std::cout << "Неверно выбранное действие" << std::endl;
+			break;
+		}
+	}
+	return 0;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	wchar_t app;
 	std::cout << "Выберете какой процесс открыть: " << std::endl;
 	std::cout << "1. Word" << std::endl;
 	std::cout << "2. Excel" << std::endl;
 	std::cout << "3. Paint" << std::endl;
 	std::cout << "4. Notepad" << std::endl;
-	char c;
-	std::cin >> c;
-	switch (c)
-	{
-	case '1':
-		StartThread(L"word.exe ");
-		break;
-	case '2':
-		StartThread(L"excel.exe ");
-		break;
-	case '3':
-		StartThread(L"mspaint.exe ");
-		break;
-	case '4':
-		StartThread(L"notepad.exe ");
-		break;
-	default:
-		std::cout << "Неверно выбранное действие" << std::endl;
-		break;
-	}
-	return 0;
+	std::cout << "5. Введите d для завершения программы" << std::endl;
+
+	menu();
 }
